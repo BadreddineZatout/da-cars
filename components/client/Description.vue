f464dcdc3d0368521984791ce8cee84683166205
BadreddineZatout <hb_zatout@esi.dz>
2024-02-25 18:45:02 +0100

    feat: add description section

---
+++ components/client/Description.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="bg-royalBlue-100 border-royalBlue-300 mb-10 flex w-full items-center justify-between border-y-2 px-10 py-10 shadow-lg lg:px-20"
+  >
+    <div class="w-full lg:w-1/2">
+      <h1 class="text-royalBlue-500 mb-1 text-3xl font-bold">Da Cars</h1>
+      <h2 class="mb-5 text-xl font-semibold">
+        Auto An- & Verkauf, Professionelle KFZ-Pflege, FOLIATEC Scheibentönung,
+        Alufelgen, Reifen, Autoglasservice, Klimaservice
+      </h2>
+      <p class="indent-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nobis
+        libero assumenda soluta similique sint ipsam perspiciatis facere numquam
+        aliquid, velit tempora non. Consectetur debitis, eum asperiores quisquam
+        doloribus necessitatibus.
+      </p>
+      <p class="indent-8">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro esse
+        doloremque veritatis aspernatur a quibusdam tempore quo blanditiis nihil
+        quisquam nam dolor et, perspiciatis non at rem ipsa, facere sapiente!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quia
+        soluta fuga voluptates. Quasi cupiditate praesentium iure et est
+        consectetur autem quam nulla quia. Vitae quaerat pariatur quae ut velit.
+      </p>
+    </div>
+    <div class="hidden w-1/2 lg:block">
+      <img src="/images/cars-garage.png" alt="cars-garage" />
+    </div>
+  </div>
+</template>
