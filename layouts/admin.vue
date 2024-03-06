3d2ad7a3011e5251b515533e697d3bc247beda4d
BadreddineZatout <hb_zatout@esi.dz>
2024-03-06 21:42:22 +0100

    feat: add sidebar in admin panel

---
+++ layouts/admin.vue
@@ -0,0 +1,11 @@
+<template>
+  <Head>
+    <Title>Admin Panel</Title>
+  </Head>
+  <div class="flex">
+    <Sidebar />
+    <slot />
+  </div>
+</template>
+
+<script setup></script>
---
+++ pages/admin/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="p-10">
+    <h1 class="text-3xl font-semibold">Welcome to the admin panel</h1>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "admin",
+});
+</script>
