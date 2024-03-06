30565adcaddc439dd8a41674ec951e4e53f200dd
BadreddineZatout <hb_zatout@esi.dz>
2024-03-06 22:20:53 +0100

    feat: add stats card in admin panel

---
+++ components/admin/Card.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="block rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+  >
+    <h5
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
+    >
+      {{ title }}
+    </h5>
+    <p class="text-3xl font-semibold text-gray-700 dark:text-gray-400">
+      {{ value }}
+    </p>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps(["title", "value"]);
+</script>
