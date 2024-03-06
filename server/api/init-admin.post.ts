1bae7f53f9018b01376c3f0a6aeaef74405ee433
BadreddineZatout <hb_zatout@esi.dz>
2024-03-06 19:55:33 +0100

    feat: add init admin user api endpoint

---
+++ server/api/init-admin.post.ts
@@ -0,0 +1,21 @@
+import prisma from "~/prisma";
+import bcrypt from "bcryptjs";
+
+export default defineEventHandler(async () => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email: "admin@test.com",
+    },
+  });
+
+  if (user) {
+    return "Admin user already exists";
+  }
+  return await prisma.user.create({
+    data: {
+      name: "Admin",
+      email: "admin@test.com",
+      password: bcrypt.hashSync("12345678", 8),
+    },
+  });
+});
