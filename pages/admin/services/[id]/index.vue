ecb101079442cde5b51de3d4c7958c7d2b08680f
BadreddineZatout <hb_zatout@esi.dz>
2024-03-09 10:24:55 +0100

    feat: add view service feature

--- pages/admin/services/[id]/index.vue
+++ pages/admin/services/[id]/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>view</div>
+  <div class="w-full p-20">
+    <div class="flex items-center justify-between">
+      <h1 class="text-3xl font-bold">Service #{{ service.id }}</h1>
+      <UButton
+        @click="handleEdit"
+        class="bg-lochmara hover:bg-blue-700"
+        label="Edit service"
+      />
+    </div>
+    <div class="mt-20 w-full">
+      <UCard>
+        <template #header>
+          <h2 class="text-2xl font-semibold">Name</h2>
+          <h3 class="mt-3 text-xl">{{ service.name }}</h3>
+        </template>
+
+        <h2 class="text-2xl font-semibold">Description</h2>
+        <p class="mt-3 indent-5 text-xl">{{ service.description }}</p>
+      </UCard>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+definePageMeta({
+  layout: "admin",
+  middleware: ["auth"],
+});
 
-<style lang="scss" scoped></style>
+const route = useRoute();
+const { data: service } = await useFetch(`/api/services/${route.params.id}`);
+
+const handleEdit = () => {
+  return navigateTo(`/admin/services/${service.value.id}/edit`);
+};
+</script>
