02ce7035f7f7387dea30582a59666bc9e9a2be6b
BadreddineZatout <hb_zatout@esi.dz>
2024-03-16 16:59:09 +0100

    chore: responsive design for admin dhasboard page

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="w-full p-10">
-    <h1 class="text-3xl font-semibold">{{ $t("welcome_admin") }}</h1>
-    <div class="mt-10 flex flex-wrap gap-x-16 gap-y-10">
+  <div class="w-full px-2 py-10 sm:px-10">
+    <h1 class="text-2xl font-semibold sm:text-3xl">
+      {{ $t("welcome_admin") }}
+    </h1>
+    <div class="mt-10 flex flex-wrap gap-5 sm:gap-x-16 sm:gap-y-10">
       <Card
-        class="w-1/4"
+        class="w-3/4 sm:w-1/4"
         :title="$t(reservation.title)"
         :value="reservation.value"
       />
-      <Card class="w-1/4" :title="$t(vehicle.title)" :value="vehicle.value" />
-      <Card class="w-1/4" :title="$t(service.title)" :value="service.value" />
+      <Card
+        class="w-3/4 sm:w-1/4"
+        :title="$t(vehicle.title)"
+        :value="vehicle.value"
+      />
+      <Card
+        class="w-3/4 sm:w-1/4"
+        :title="$t(service.title)"
+        :value="service.value"
+      />
     </div>
   </div>
 </template>
